cd40dacfb98bf888bd04e1720c55570a420af669
norahl73 <mbv7xs@virginia.edu>
2025-08-15 17:51:59 -0400

    got quiz logic down, can go back and forth between questions, answer is recorded, score is recorded, needs start page + styling now

--- src/components/Quiz/quiz.tsx
+++ src/components/Quiz/quiz.tsx
@@ -1,9 +1,56 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./quiz.css";
 import logo from "../../assets/logo.png";
-// import { questions, getQuizResult } from "../../assets/logic";
 
 const Quiz = () => {
+  let totalScore = 0;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState(Array(questions.length).fill(null));
+  const [isFinished, setIsFinished] = useState(false);
+
+  // once answer is chosen, add the score to totalScore
+  // create a shallow copy of the answers state array and stores answer user has chosen
+  const handleChoice = (answerIndex) => {
+    totalScore += Object.values(
+      questions[currentQuestion].answers[answerIndex]
+    )[0];
+    const newAnswers = [...answers];
+    newAnswers[currentQuestion] = answerIndex;
+    setAnswers(newAnswers);
+  };
+
+  const nextQuestion = () => {
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+    } else {
+      setIsFinished(true);
+    }
+  };
+
+  const prevQuestion = () => {
+    if (currentQuestion > 0) {
+      setCurrentQuestion((prev) => prev - 1);
+    }
+  };
+
+  function getQuizResult() {
+    if (totalScore <= 10) {
+      return "Pheasant";
+    } else if (11 <= totalScore && totalScore <= 20) {
+      return "Pangolin";
+    } else if (21 <= totalScore && totalScore <= 30) {
+      return "Deer";
+    } else if (31 <= totalScore) {
+      return "Viper";
+    }
+  }
+
+  if (isFinished) {
+    let fam = getQuizResult();
+    fam += ".png";
+    return <ResultPage fam={fam} />;
+  }
+
   return (
     <>
       <div className="container">
@@ -16,8 +63,126 @@ const Quiz = () => {
           <button>Next</button>
         </div>
       </div>
+      <div>
+        <h2>{questions[currentQuestion].question}</h2>
+        {questions[currentQuestion].answers.map((opt, idx) => (
+          <button
+            key={idx}
+            onClick={() => handleChoice(idx)}
+            style={{
+              background:
+                answers[currentQuestion] === idx ? "lightblue" : "white",
+            }}
+          >
+            {Object.keys(opt)[0]}
+          </button>
+        ))}
+        <div>
+          <button onClick={prevQuestion} disabled={currentQuestion === 0}>
+            Back
+          </button>
+          <button onClick={nextQuestion}>Next</button>
+        </div>
+      </div>
     </>
   );
 };
+
+const questions = [
+  // question 1
+  {
+    question: "How early will you depart",
+    image: "./logo.png",
+    answers: [
+      { "Early, who knows if I might get lost!": 3 },
+      { "Calculate to be just on time": 7 },
+      { "Take your time—it’s not that deep": 1 },
+      { "Whenever I feel like it": 9 },
+    ],
+  },
+
+  // question 2
+  {
+    question: "What will you wear",
+    image: "./logo.png",
+    answers: [
+      { "Cute, gotta doll up": 4 },
+      { "Cool, must maintain aura": 8 },
+      {
+        "Professional, never know what connections I can make": 5,
+      },
+      { "I dont gaf": 2 },
+    ],
+  },
+
+  // question 3
+  {
+    question: "How will you get there",
+    image: "./logo.png",
+    answers: [
+      { "Walk, gotta get my steps in": 1 },
+      { "Bus, I'm nawt walking": 6 },
+      { "Uber, no plebs in my area": 3 },
+      { "Veo, …": 7 },
+    ],
+  },
+
+  // question 4
+  {
+    question: "See someone with their backpack open, wyd?",
+    image: "./logo.png",
+    answers: [
+      { "Pretend you don’t see": 2 },
+      { "Laugh, that’s embarrassing haha": 9 },
+      { "Tell them, can’t do them like that": 5 },
+      { "Try to zip it back up… but backfires": 6 },
+    ],
+  },
+
+  // question 5
+  {
+    question: "You get there, wyd",
+    image: "./logo.png",
+    answers: [
+      { "Sit down right away": 3 },
+      { "Wait for friends": 7 },
+      { "Go on phone": 8 },
+      { "Start talking to new people": 1 },
+    ],
+  },
+
+  // question 6
+  {
+    question: "What boba flavor ru getting?",
+    image: "./logo.png",
+    answers: [
+      { "Thai Tea": 4 },
+      { "Passionfruit Tea": 6 },
+      { Water: 2 },
+      { Nothing: 9 },
+    ],
+  },
+
+  // question 7
+  {
+    question: "After gbm where u going",
+    image: "./logo.png",
+    answers: [
+      { Volleyball: 7 },
+      { "Yap with the people": 1 },
+      { "Explore Newcomb Ballroom": 8 },
+      { "Go straight home": 3 },
+    ],
+  },
+];
+
+function ResultPage({ fam }) {
+  return (
+    <div>
+      <h1>Your Fam is...</h1>
+      <img src={fam} />
+    </div>
+  );
+}
 
 export default Quiz;
